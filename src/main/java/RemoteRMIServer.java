8d1dac67e2813c1c9f58cb028875bb933de338cf
Crypt0n <spencer_clay@foxmail.com>
2022-08-13 09:37:11 +0800

    提交代码，但是报access denied java.net.SocketPermission错误，可能是没有正确加载client.policy，以后再试试

---
+++ src/main/java/RemoteRMIServer.java
@@ -0,0 +1,26 @@
+import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+public class RemoteRMIServer {
+
+    private void start() throws Exception {
+        if (System.getSecurityManager() == null) {
+            System.out.println("setup SecurityManager");
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        Calc h = new Calc();
+        LocateRegistry.createRegistry(12099);
+        Naming.rebind("refObj", h);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new RemoteRMIServer().start();
+    }
+
+}
